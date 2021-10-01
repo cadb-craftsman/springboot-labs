1f3b49e7bd98420f89b6cb6f565de87df05fac36
carlos <caaldi@ext.amaseguros.com>
2021-10-01 14:36:02 +0200

    First commit of the initial code.

---
+++ mssc-brewery/src/main/kotlin/com/woowrale/msscbrewery/MsscBreweryApplication.kt
@@ -0,0 +1,11 @@
+package com.woowrale.msscbrewery
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MsscBreweryApplication
+
+fun main(args: Array<String>) {
+	runApplication<MsscBreweryApplication>(*args)
+}
---
+++ mssc-brewery/src/test/kotlin/com/woowrale/msscbrewery/MsscBreweryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.woowrale.msscbrewery
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MsscBreweryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
