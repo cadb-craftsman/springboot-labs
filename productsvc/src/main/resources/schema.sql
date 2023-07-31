14a6a039edf022697ada57644ca6bf0a13b17223
Carlos Diaz <cadb.craftsman@gmail.com>
2023-07-31 07:32:07 -0500

    update repository

--- productsvc/src/main/resources/schema.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE IF EXISTS product;
- 
-CREATE TABLE product (id int primary key auto_increment, name varchar, description varchar);
-
--- INSERT INTO PRODUCT (id, name, description) VALUES (1,'mobile','object mobile');
